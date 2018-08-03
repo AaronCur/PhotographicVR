2f5bbb721b191b1e6f93b65101ef8c60cbfb5931
AaronCur <C00202505@itcarlow.ie>
2018-08-03 14:56:26 +0100

    Added edit and upload fixes

--- public/js/upload.js
+++ public/js/upload.js
@@ -39,15 +39,7 @@ function getCourse() {
 }
 
 function setHeader() {
-  var holes = JSON.parse(localStorage.getItem('holes'));
 
-  if (holes < 10) {
-    document.getElementById("pictures_div").style.display = "block";
-    document.getElementById("holes_div").style.display = "none";
-  } else {
-    document.getElementById("pictures_div").style.display = "none";
-    document.getElementById("holes_div").style.display = "block";
-  }
 
   var course = localStorage.getItem('course');
   document.getElementById("course_head").innerHTML = "Upload to " + JSON.parse(course);
